7aa72a5f73fd183ee60ceea40e58e4f7f71a3b81
Yung-Ju Chang <mr.armuro@gmail.com>
2013-10-09 12:36:36 -0400

    Create the User model (keep testing)

---
+++ Wousay/Models/Users.h
@@ -0,0 +1,13 @@
+//
+//  Users.h
+//  Wousay
+//
+//  Created by Armuro on 10/9/13.
+//  Copyright (c) 2013 Armuro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Users : NSObject
+
+@end
---
+++ Wousay/Models/Users.m
@@ -0,0 +1,13 @@
+//
+//  Users.m
+//  Wousay
+//
+//  Created by Armuro on 10/9/13.
+//  Copyright (c) 2013 Armuro. All rights reserved.
+//
+
+#import "Users.h"
+
+@implementation Users
+
+@end
